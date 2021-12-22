97c35d2761d3f92f2dcb5d7510dc7da04222c96f
jinan159 <wlsdhks0423@naver.com>
2021-12-22 23:00:52 +0900

    MemberForm name을 null, "", " " 모두 검증하도록 변경
    기존 NotEmpty는 " "(공백)일 경우 검증을 통과하는데, 이를 통과하지 않도록 변경

--- src/main/java/jpabook/jpashop/controller/dto/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/dto/MemberForm.java
@@ -5,12 +5,12 @@ import jpabook.jpashop.domain.Member;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 이름은 필수 입니다")
+    @NotBlank(message = "회원 이름은 필수 입니다")
     private String name;
 
     private String city;
